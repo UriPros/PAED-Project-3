9de2c0d5bc2fbf6f9137d6c3778814bfc987922e
UriPros <loiropros@gmail.com>
2025-12-19 12:02:03 +0100

    Create tableLL.c

---
+++ tableLL.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "table.h"
+#include "tableLL.h"
+
+ElementNode* list_add(ElementNode* head, Element e) {
+    ElementNode* newNode = malloc(sizeof(ElementNode));
+    newNode->data = e;
+    newNode->next = head;
+    return newNode;
+}
+
+Element* list_get(ElementNode* head, char* name) {
+    while (head != NULL) {
+        if (strcmp(head->data.name, name) == 0) {
+            return &head->data;
+        }
+        head = head->next;
+    }
+    return NULL;
+}
+
+ElementNode* list_remove(ElementNode* head, char* name) {
+    ElementNode* curr = head;
+    ElementNode* prev = NULL;
+
+    while (curr != NULL) {
+        if (strcmp(curr->data.name, name) == 0) {
+            if (prev == NULL)
+                head = curr->next;
+            else
+                prev->next = curr->next;
+
+            free(curr);
+            return head;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    return head;
+}
+
+void list_free(ElementNode* head) {
+    while (head != NULL) {
+        ElementNode* temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
